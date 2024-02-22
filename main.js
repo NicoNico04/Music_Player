18b5cd202f0a2d28c8c73cfabb3d6bf159eba42d
NicoNico04 <nicollcanocastro465@gmail.com>
2024-02-22 16:25:22 -0500

    Js Music Player

--- main.js
+++ main.js
@@ -6,8 +6,60 @@ document.querySelector(".Library").addEventListener("click", () =>{
     main.classList.toggle("main_toggle")
 })
 
-const songs = [ 
-    
-]
+/*Importar canciones y recorer la lista*/
 
+import {songs} from "./songs.js"
+
+let songs_info = " "
+
+songs.forEach((song) =>{
+    songs_info += `
+    <article class="por_reproducir" id="${song.id}">
+        <img src="${song.img_song}" alt="">
+            <section class="SONG_DESCRIPTION">
+                <h1>${song.title}</h1>
+                <p>${song.artist}</p>
+            </section>
+    </article>`
+})
+
+Aside.innerHTML = songs_info
+
+
+/*Canciones en el aside id*/
+const por_reproducir = document.querySelectorAll(".por_reproducir")
+const main_s = document.querySelector(".main_song")
+
+por_reproducir.forEach((article) =>{
+
+    article.addEventListener("click", () => {
+        let ides = article.id 
+        console.log(ides)
+        const cs = songs.find((song) => song.id == ides)
+        console.log(cs)
+
+        /*Cancion en el main*/
+        let song_main = `
+            <img src="${cs.img_song}" alt="" class="img_song">
+            <h1>${cs.title}</h1>
+            <h4>${cs.artist}</h4>
+            <audio src="${cs.song}"></audio>`
+        main_s.innerHTML = song_main
+
+        const audio = document.querySelector('audio')
+        audio.play()
+    })
+
+})
+
+
+/*
+const controllers = () => {
+
+} 
+play = document.getElementById(".pausa");
+play.addEventListener("click",()=>{
+    Audio.play();
+}
+); */
 
---
+++ songs.js
@@ -0,0 +1,46 @@
+export const songs = [ 
+    {
+        id : "1",
+        title : "Gitana",
+        artist : "Willie Colon",
+
+        img_song : "https://lh3.googleusercontent.com/pI7_1l0wUgPTyiPOAwPOI0DcM2mYESlpOzml8sWpX9WUvnMRAv6xvDFGb-cVarWxgtuMzS7sova_c_b8=w544-h544-l90-rj",
+        song : "./songs/Gitana.mp3"
+    },
+
+    {
+        id : "2",
+        title : "Mujer divina",
+        artist : "Joe Cuba y su Sexteto",
+
+        img_song : "https://lh3.googleusercontent.com/qJSshccwUuolsxDUhrO34Nvy82DXC2vd9x1pTUdFEqS_6SHqr-0PpOCQmnyQ3mzVqqZxqWwl-H92Qbq7kA=w544-h544-l90-rj",
+        song : "./songs/Mujer Divina.mp3"
+    },
+
+    {
+        id : "3",
+        title : "El muñeco de la ciudad",
+        artist : "Bobby Valentin",
+
+        img_song : "https://lh3.googleusercontent.com/PoSCq1CarqAuontEPCrhND3LSoHE3b-YVP5sNtmxxsbx0oV5kjVFCaraKwb-EXSv37iA1ig4TzEe7fCymA=w544-h544-l90-rj",
+        song : "./songs/El Muñeco de la Ciudad.mp3"
+    },
+
+    {
+        id : "4",
+        title : "Oh que sera",
+        artist : "Willie Colon",
+
+        img_song : "https://lh3.googleusercontent.com/8m4vvtRy9JYGVC0zWythB6Zo9VUJZKqtPfmp6Q5MDxb9M5ScCKWLkffLe10lDiqAdQDibzxnDZ9RixU=w544-h544-l90-rj",
+        song : "./songs/Oh Que Sera_.mp3"
+    },
+
+    {
+        id : "5",
+        title : "Amarilla se pone",
+        artist : "Orquesta Fuerza Mayor",
+
+        img_song : "https://lh3.googleusercontent.com/KMEr0anjGtKpnoJP0as81LlSrXz32qe2_loZ-o8MYk36KaD9RmtaOXJ9cGzFwoDjIdR8hkskqy37HTQ=w544-h544-l90-rj",
+        song : "./songs/Amarilla Se Pone.mp3"
+    }
+]
