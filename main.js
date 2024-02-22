2799b35f446abc1ace1b02ab161047b2ef27cd62
NicoNico04 <nicollcanocastro465@gmail.com>
2024-02-22 11:47:55 -0500

    Desing Music Player

---
+++ main.js
@@ -0,0 +1,13 @@
+const Aside = document.querySelector(".songs")
+const main = document.querySelector(".main")
+
+document.querySelector(".Library").addEventListener("click", () =>{
+    Aside.classList.toggle("aside_toggle")
+    main.classList.toggle("main_toggle")
+})
+
+const songs = [ 
+    
+]
+
+
